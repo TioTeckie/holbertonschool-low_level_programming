736d602105129d9ad08bcbdd7f8deb7bf3260e5d
jj2024012024@gmail.com <jj2024012024@gmail.com>
2025-06-17 22:14:46 -0400

    to fix betty comp

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,7 +5,7 @@
 * This function is the entry point of the program.
 * Return: Always 0 (Success)
 */
-int main(void)
+int main(void) /* entry point*/
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
